8a5a55d6df0c8b98b9762ead3d5f296a4e3fda95
Sean Wilson <seaniewill@me.com>
2013-04-19 11:41:55 -0400

    Finished the homework assignment HW_01.rb

--- HW_01.rb
+++ HW_01.rb
@@ -1,65 +1,69 @@
-###############################################################################
-#
-# Introduction to Ruby on Rails
-#
-# Lab 01
-#
-# Purpose:
-#
-# Read the links below and complete the exercises in this file. This Lab
-# is to test your knowledge of Variables and Conditionals.
-#
-###############################################################################
-#
-# 1. Welcome users to your game. Let them know you are the creator.
-#
-# 2. Ask the user for their name and store it in a variable.
-#
-# 3. Personally greet the player by printing to the screen, "Hi player_name!
-#	 Let them know they have 3 guesses to guess the Secret Number that is between 1 and 10.
-#
-# 4. Create a new Integer variable called `guesses_left`, this will count
-#    down how many more times the Player can guess. It's initial value should
-#    be 3.
-#
-# 5. Using String Interpolation, concatenate the variable `guesses_left`
-#    into a String that tells the Player how many guesses they have left
-#    and print it to the screen.
-#
-# 6. Create a new Integer variable called `secret_number` and set the value to
-#    a number of your choosing between 1 and 10. This is the number that
-#    our Player will try to guess.
-#
-# 7. Ask the Player to make their first guess. (Remember to cast input from the Player into the appropriate
-#    data type).
-#
-# 8.  Use a Conditional to find out if the Player has guessed the
-#     correct number.
-#
-#     1. If they guessed correctly, tell them they won and exit the
-#        game.
-#
-#	  2. For an incorrect guess decrement the variable `guesses_left` by 1 and
-#     print to the screen how many guesses the Player has left.
-#
-#     		1. If they guessed a number too low, tell them that they need to
-#        	   guess higher on their next guess.
-#
-#     		2. If they guessed a number too high, tell them that they need to
-#        	   guess lower on their next guess.
-#
-# 9. Repeat the above process for a total of 3 times asking the Player
-#     to make a guess and verify if the Player won.
-#
-# 10. If they do not make the correct guess after 3 tries, print "Game Over" and
-#     let the player know the `secret_number`.
-#
-# 11. Make sure to add helpful comments to your code to document what
-#     each section does.
-#
-###############################################################################
-#
-# Student's Solution
-#
-###############################################################################
+# Sean Wilson - HW_01
+
+# Welcoming the user
+puts "Welcome to my game. I am the creator, Sean"
+
+# Creating the variable for the player's name and introducing the game
+puts "What is your name"
+player_name = gets.chomp
+puts "Hi #{player_name}, You will have 3 guesses to guess the Secret Number that is between 1 and 10"
+
+# Creating the guesses_left variable, secret number variable and telling them how many guess they have
+guesses_left = 3
+puts "You have #{guesses_left} guesses left."
+secret_number = 5
+
+# Starting the game by asking their guessess and processing it
+puts "Take your first guess"
+first_guess = gets.chomp
+
+# Processing first guess
+if first_guess.to_i == secret_number.to_i
+  puts ("You have won")
+  exit
+else
+  guesses_left = guesses_left - 1
+  puts "#{player_name}, you have guessed incorrectly, you have #{guesses_left} more guesses left."
+  if first_guess.to_i < secret_number.to_i
+      puts "Your guess was too low"
+  else
+      puts "Your guess was too high"
+  end
+end
+
+# Second guess
+puts "Go ahead and take your second guess"
+second_guess = gets.chomp
+if second_guess.to_i == secret_number.to_i
+  puts ("You have won")
+  exit
+else
+  guesses_left = guesses_left - 1
+  puts "#{player_name}, you have guessed incorrectly, you have #{guesses_left} more guesses left."
+  if second_guess.to_i < secret_number.to_i
+      puts "Your guess was too low"
+  else
+      puts "Your guess was too high"
+  end
+end
+
+# Third guess
+puts "Try one last time!"
+third_guess = gets.chomp
+if third_guess.to_i == secret_number.to_i
+  puts ("You have won")
+  exit
+else
+  guesses_left = guesses_left - 1
+  puts "#{player_name}, you have guessed incorrectly, you have #{guesses_left} more guesses left."
+  if third_guess.to_i < secret_number.to_i
+      puts "Your guess was too low"
+  else
+      puts "Your guess was too high"
+  end
+end
+
+# End of game, they lost
+puts "Game Over, the secret number is #{secret_number}"
+
 
