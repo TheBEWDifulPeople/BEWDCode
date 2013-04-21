05f8383ab6cbe1efcf8daf00da63241587ffab0f
Joseph Torreggiani <joseph.torreggiani@gmail.com>
2013-04-21 14:57:35 -0400

    This is the initial commit for Homework 1

--- HW_01.rb
+++ HW_01.rb
@@ -63,3 +63,104 @@
 #
 ###############################################################################
 
+# Creator: Joseph Torreggiani        
+# Date: 04/21/2013
+
+###############################################################################
+
+# Variable Declarations 
+
+#Get a random number between 1 through 10 and store it.
+secret_number = rand(1..10) 
+
+#Create a variable for the number of guesses left.
+guess_left = 3
+
+#Create a variable for the user's name.
+user_name = "Username"
+
+###############################################################################
+
+# Introduction 
+
+print "\n\n\n"           
+
+print "Welcome to the Secret Number game!\n\n"
+
+print "This game was created by Joseph Torreggiani.\n\n" 
+
+print "What is your name?\n"
+
+# Get the user's name. 
+user_name = gets.chomp
+
+print "\n\nHello #{user_name.capitalize}!\n\n"
+
+ 
+###############################################################################
+
+# Game Instructions 
+
+print "You have " + guess_left.to_s + " guesses to determine the Secret Number.\n
+The Secrete Number is between 1 and 10.\n\n"
+
+print "Make your first guess?\n\n"
+
+#Get the user's guess.
+user_guess = gets.to_i
+
+###############################################################################
+
+# Create a loop that checks the user's guesses.
+loop do
+	
+# Check if the user's guess is correct. If correct break the loop and end the game.
+	if user_guess == secret_number 
+		print "\nYou guessed correctly #{user_name.capitalize}!\n\n"
+		print "Game Over\n\n" 
+		break
+	end
+
+# Check if the user's guess is less than the secret number.
+	if user_guess < secret_number
+		print "\nGuess higher.\n\n" 
+
+# Calculate the number of guesses left.
+		guess_left = guess_left - 1
+		
+# Check if there are any guesses left. 
+			if guess_left == 0 
+				print "\n\nYou have no guesses left.\n\n"
+				print "\n\nGame Over\n\n "
+				break
+			end
+# Tell the user how many guesses are left and guess again.
+		print "You have #{guess_left} guesses left.\n\n" 
+		print "Guess again.\n\n"
+
+# Get the user's guess.
+		user_guess = gets.to_i
+	end
+
+# Check if the user's guess is greater than the secret number.
+	if user_guess > secret_number
+		print "\nGuess lower.\n\n"
+
+# Calculate the number of guesses left.
+		guess_left = guess_left - 1
+
+# Check if there are any guesses left. 
+			if guess_left == 0 
+				print "\n\nYou have no guesses left.\n\n"
+				print "\n\nGame Over\n\n "
+				break
+			end
+
+# Tell the user how many guesses are left and to guess again.
+		print "You have #{guess_left} guesses left.\n\n" 
+		print "Guess again.\n\n"
+		user_guess = gets.to_i
+
+	end
+	
+end
