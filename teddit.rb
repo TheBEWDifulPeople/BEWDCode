be32e1dfdac43c92cb2cba3cd6a1c0c677b01b88
Raymond Dias <rdias23@gmail.com>
2013-04-22 12:01:54 -0400

    added code we wrote in class

--- teddit.rb
+++ teddit.rb
@@ -1,2 +1,21 @@
-print "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
-print "More stuff"
+puts "Welcome to Teddit! a text based news aggregator. Get today\'s news tomorrow!"
+puts "Story: Man raise by wolverines, then eaten Current upvotes: 4"
+story = "Cat makes fortune by showing elderly how to play dice"
+upvotes = 7
+
+puts 'Story: ' + story + ' Current upvotes: ' + upvotes.to_s
+puts "Story: #{story.capitalize.reverse} Current upvotes: #{upvotes}"
+
+def show_story(story, upvotes)
+	puts "Story: #{story.capitalize} Current upvotes: #{upvotes}"
+end
+show_story "GA opens new school", 70
+show_story "BEWS Class learned ruby 100% the first day", 1000
+
+def encourage
+	puts "You're doing great"
+end
+
+encourage
+encourage
+encourage
