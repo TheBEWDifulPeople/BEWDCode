1d9f7951d9e21202f4838d4fdefdb7938e303570
Adam Freemer <a@freemer.com>
2013-04-23 18:45:31 -0400

    Update Repo

---
+++ teddit.rb
@@ -0,0 +1,15 @@
+puts "Heeeello World, welcome to Teddit's home"
+puts "Story: Man raised by wolverines, then eaten Current upvotes: 4"
+story = "Cat makes a fortune by showing elderly how to play dice"
+upvotes = 7
+puts "story: " + story + " Current upvotes: " + upvotes.to_s
+puts "Story: #{story.capitalize.reverse} Current upvotes: #{upvotes}"
+
+def show_story(story, upvotes)
+	puts "Story: #{story.capitalize.reverse} Current upvotes: #{upvotes}"	
+end
+show_story "GA opens da new skool", 85
+show_story "GA opens da new skool", 200
+
+#comment
+
