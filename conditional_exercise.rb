57e0642bac51c19985e96ef0e40cf42e5676161c
Piotr Kuczynski <piotr@danzigstudio.com>
2013-04-24 22:34:39 -0400

    commit 4/24

--- conditional_exercise.rb
+++
@@ -1,6 +0,0 @@
-# Copy the code from the strings_exercise_SN_solution.rb
-#
-# New Feature! If the Story is about cats multipy the upvotes by 5
-# If the Story is about bacon multiply the upvotes by 8
-# Cats frolic inspite of a tuna shortage should give you 5 times the upvotes!
-# If the category is Food it gets 3 times the upvotes
