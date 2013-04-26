25b44f0f0fde463ad6b4d2822269e47ad5cc5988
connorlee <connorlee57@gmail.com>
2013-04-25 21:01:42 -0400

    added has ex

--- hashes_exercise.rb
+++ hashes_exercise.rb
@@ -7,3 +7,50 @@
 # Add this hash to an array called stories and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
 # Through the stories array 
 # Test your cat, bacon, and food upvote upgrades work.
+stories= []
+
+def show_message(message)
+    puts message
+end
+
+def get_input
+    gets.strip
+end
+
+def show_new_story_notification(story, category, upvotes)
+    show_message("New story added! #{story}, Category: #{category.capitalize}, Current Upvotes: #{upvotes}")
+end
+
+story = {}
+
+def calculate_upvotes(story)
+   story[:upvotes] = 1
+
+upvotes = 1
+story[:upvotes] = upvotes
+
+
+show_message("Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!")
+show_message("Please enter a News story:")
+
+story[:title] = get.input
+if story[:title].downcase.include?("cats")
+	story[:upvotes] *= 5
+end
+
+if story.downcase.include?("bacon")
+	story[:upvotes] = 8
+end
+
+show_message("Please give it a category:")
+
+story[:category] = get_input
+
+if story == "Cats frolic inspite of a tuna shortage"
+	upvotes *= 5
+end
+
+if category.downcase.include?("food")
+ 	upvotes *= 3
+end
+
