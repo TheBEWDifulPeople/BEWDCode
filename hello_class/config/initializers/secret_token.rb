4b9c6a81abd1c456e1d580590e5758ad037749b6
Justin Belmont <justinbelmont@gmail.com>
2013-05-11 16:50:51 -0400

    Rewsly HW

---
+++ hello_class/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloClass::Application.config.secret_key_base = '58f204a9755c92f0b5e29f2a03a40616bb2ee426c77cc51af3e9829a4f46aecd397ca5ab7d3424f724015785dc517ee7f8142664d54c241ba0d94dd3d896cdaf'
