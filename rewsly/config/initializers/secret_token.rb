ab97f3b2afd1707d763d8ab7763a59bb7e5d2294
Raymond Dias <rdias23@gmail.com>
2013-05-12 14:27:30 -0400

    adding rewsly homework

---
+++ rewsly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rewsly::Application.config.secret_key_base = '04f32aa96b6ef7c02fdd0fe02f0c37bb9bbf46fd9037b9aaa7219c79242441984fe11b8ee445fd6ffe3f4854cff772cf66ad850b5b08e29a468fe6c40d1a67a0'
