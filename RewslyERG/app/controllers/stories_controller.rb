2c3e265a983df694c0261887cb735e6f323eadcd
Ely <Ely@Elys-MacBook-Pro.local>
2013-05-14 08:00:35 -0400

    Rewsly for HW 14 May

---
+++ RewslyERG/app/controllers/stories_controller.rb
@@ -0,0 +1,9 @@
+class StoriesController < ApplicationController
+
+	def index
+		@stories = ["Dogs used to treat restless leg syndrome.", "Another story coming soon."]
+	end
+
+end
+
+
---
+++ RewslyERG/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RewslyERG::Application.config.secret_key_base = 'be13d2c0ea382ecf34f5b146521caaaf1b8f89c042385b459ba7813e48d9995f95ec34e24fb5b88718063c74e393500f913fd5468bd07ea7dedf5ee0b0ebd21e'
