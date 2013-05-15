fab83c62c0c091813e1fb592f3b63ce07875872c
Chris Bosco <chris@masterstreet.com>
2013-05-15 00:47:00 -0400

    Rspec test

--- midterm_chris_bosco/spec/craigslist_source_spec.rb
+++ midterm_chris_bosco/spec/craigslist_source_spec.rb
@@ -1,26 +1,13 @@
 require_relative '../lib/craigslist_source'
 
-before_each do
-end
-
-describe Story do 
-  it "Does not accept empty subdomain" do
-    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
-    story.title.must_equal "Wombat sees dentist after 10 year hiatus"
-    story.category.must_equal "Nature"
-  end
-
-  it "has its upvotes set to 1 by default" do
-    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
-    story.upvotes.must_equal  1
-  end
-
-  it "can be upvoted and downvoted" do
-    story = Story.new "Wombat sees dentist after 10 year hiatus", "Nature"
-    story.upvote
-    story.upvotes.must_equal 2
-
-    story.downvote
-    story.upvotes.must_equal 1
-  end
+describe CraigslistSource do 
+    it "Rejects an invalid region" do
+        cs = CraigslistSource.new
+        expect{cs.subdomain = "milkyway"}.to raise_error RuntimeError
+    end
+    it "Accepts a valid region" do
+        cs = CraigslistSource.new
+        cs.subdomain = "newyork"
+        cs.subdomain.should eq("newyork")
+    end
 end
