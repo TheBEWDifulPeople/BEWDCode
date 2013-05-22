c0bbffda229d8f3ab66d10e53e7646e2fe665908
Brad Weinberg <bradweinberg@ubuntu.(none)>
2013-05-21 18:29:18 -0700

    commite

--- 5 - Objects/creating_objects_code_along.rb
+++ 5 - Objects/creating_objects_code_along.rb
@@ -73,10 +73,10 @@
 #####################################################################################################
 #####################################################################################################
 #####################################################################################################
-#
+
 # class Student
 #   attr_accessor :name, :major, :course, :grade
-# 
+
 #   def grade_status
 #     if @grade == "F"
 #       "failed"
@@ -87,26 +87,26 @@
 #     end
 #   end
 # end
-# 
+
 # jimmy = Student.new
 # jimmy.name = "Jimmy Mazzy"
 # jimmy.major = "Math"
 # jimmy.course = "Math"
 # jimmy.grade = "A"
-# 
+
 # pepe = Student.new
 # pepe.name = "Pepe Phaenagrotis"
 # pepe.major = "Music"
 # pepe.course = "Math"
 # pepe.grade = "C"
-# 
+
 # edward = Student.new
 # edward.name = "Edward Ellis"
 # edward.major = "Math"
 # edward.course = "Math"
 # edward.grade = "C"
-# 
-# 
+
+
 # puts "#{jimmy.name} has #{jimmy.grade_status} #{jimmy.course}"
 # puts "#{pepe.name} has #{pepe.grade_status} #{pepe.course}"
 # puts "#{edward.name} has #{edward.grade_status} #{edward.course}"
@@ -114,25 +114,35 @@
 # One more thing though...
 # I want this to work
 
-# class Student
-#   attr_accessor :name, :major, :course, :grade
-# 
-#   def grade_status
-#     if @grade == "F"
-#       "failed"
-#     elsif @major == @course && @grade >="C"
-#       "falied"
-#     else
-#       "passed"
-#     end
-#   end
-# end
-#
-# jimmy = Student.new("Jimmy Mazzy", "Math", "Math", "A")
-# pepe = Student.new("Pepe Phaenagrotis", "Music", "Math", "C")
-# edward = Student.new("Edward Ellis", "Math", "Math", "C")
+class Student
+  attr_accessor :name, :major, :course, :grade
 
-# puts jimmy
-# puts pepe
-# puts edward
+  def initialize(name,major,course,grade)
+  	@name = name
+  	@major = major
+  	@course = course
+  	@grade = grade
+  end
 
+  def to_s
+  	"#{@name} has #{grade_status} #{@course}"
+  end
+
+  def grade_status
+    if @grade == "F"
+      "failed"
+    elsif @major == @course && @grade >="C"
+      "falied"
+    else
+      "passed"
+    end
+  end
+end
+
+jimmy = Student.new("Jimmy Mazzy", "Math", "Math", "A")
+pepe = Student.new("Pepe Phaenagrotis", "Music", "Math", "C")
+edward = Student.new("Edward Ellis", "Math", "Math", "C")
+
+puts jimmy
+puts pepe
+puts edward
